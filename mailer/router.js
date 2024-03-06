6e360c4bfdefb711bef9eb59e70248001c7729e0
Yousra Khurshid <YousraKhurshid1@gmail.comgit>
2024-03-07 00:04:43 +0500

    https://github.com/YousraKhurshid/WD-2-FinalProject.git

--- mailer/router.js
+++
@@ -1,7 +0,0 @@
-const app = require('express')
-const router = app.Router()
-const { sendMail, sendFancyMail } = require('./controller')
-
-router.post('/sendmail', sendMail)
-router.post('/sendfancymail', sendFancyMail)
-module.exports = router
